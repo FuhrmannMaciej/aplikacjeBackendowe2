d659e3113a2c4be4f626ad370f917b7cded1a860
maciej.fuhrmann <maciej.fuhrmann@happyteam.io>
2022-03-27 12:55:17 +0200

    Added addUser and fixed other methods

--- src/main/java/com/example/demo/UsersController.java
+++ src/main/java/com/example/demo/UsersController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -11,9 +12,11 @@ import java.util.Map;
 @Controller
 public class UsersController {
 
-    private Map<Integer, UserEntity> users = Map.of(1,new UserEntity("john", 21),
-                                                    2,new UserEntity("tom", 18),
-                                                    3,new UserEntity("paul", 42));
+    private Map<Integer, UserEntity> users = new HashMap<>(){{
+                        put(1,new UserEntity("john", 21));
+                        put(2,new UserEntity("tom", 18));
+                        put(3,new UserEntity("paul", 42));
+    }};
 
 
     @RequestMapping("/users")
@@ -30,8 +33,16 @@ public class UsersController {
 
     @RequestMapping("/users/{id}/remove")
     @ResponseBody
-    public String removeUser(@PathVariable int id) {
-        users.remove(id);
-        return "User deleted";
+    public Object removeUser(@PathVariable int id) {
+        return users.remove(id);
+    }
+
+    //http://localhost:8080/users/add?name=matt&age=35
+    @RequestMapping("/users/add")
+    @ResponseBody
+    public Object addUser(@RequestParam String name, @RequestParam int age) {
+        int size = users.size() + 1;
+        users.put(size, new UserEntity(name, age));
+        return users.get(size);
     }
 }
